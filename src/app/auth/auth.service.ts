ed699d6e4cf10dffe1f1cc75f9e73a3d7dfc124f
Paras Choudhary <paras.choudhary@ibm.com>
2021-07-13 18:57:05 +0530

    removed backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {User} from "./model/user.model";
 
 
@@ -9,12 +9,18 @@ import {User} from "./model/user.model";
 @Injectable()
 export class AuthService {
 
+  user:any;
     constructor(private http:HttpClient) {
 
     }
 
     login(email:string, password:string): Observable<User> {
-        return this.http.post<User>('/api/login', {email,password});
+        if(email=='paras@gmail.com' && password == 'test'){
+          this.user= {id:1, email:'paras@gmail.com'}
+          return of( this.user );
+
+
+        }
     }
 
 }
